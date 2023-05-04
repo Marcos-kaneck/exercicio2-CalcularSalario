30203ce044ae7828371427010356590133f206da
Marcola <51445096+Marcos-kaneck@users.noreply.github.com>
2023-05-04 16:44:32 -0400

    Add files via upload

---
+++ exercicio2/src/entities/Employee.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author User
+ */
+public class Employee {
+
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public double netSalary() {
+        return grossSalary - tax;
+    }
+
+    public void increaseSalary(double percetage) {
+
+        this.tax += grossSalary % percetage;
+    }
+
+    public String toString() {
+        return name + ", $ " + String.format("%.2f", tax);
+    }
+}
---
+++ exercicio2/src/exercicio2/Exercicio2.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio2;
+
+
+import entities.Employee;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Exercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Employee emp = new Employee();
+        
+        System.out.println("Name: ");
+        emp.name = sc.nextLine();
+        System.out.println("Gross Salary: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.println("Tax: ");
+        emp.tax = sc.nextDouble();
+        
+        System.out.print("Employee: "+emp);
+        
+        System.out.println("Which pertentage to increase salary? ");
+        double salary = sc.nextDouble();
+        emp.increaseSalary(salary);
+        
+        System.out.print("Upadated data: "+emp);
+    }
+    
+}
